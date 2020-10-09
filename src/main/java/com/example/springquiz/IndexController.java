737bdbabe8640b47e50b9497689b95a2fb5e6adb
alexret <alex.relano18@gmail.com>
2020-10-09 11:58:04 +0200

    Correción de errores

--- src/main/java/com/example/springquiz/IndexController.java
+++ src/main/java/com/example/springquiz/IndexController.java
@@ -1,18 +1,22 @@
 package com.example.springquiz;
 
+import java.io.File;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController {
 	
 	@RequestMapping("/index")
-	  public String hola(Model modelo) {
-	    
-	    
-	    modelo.addAttribute("mensaje","hola desde thymeleaf");
+	  public String  get_valor(Model modelo) {
+		
+	    		    	
 	    return "index";
 	}
+	
+	
 
 }
