a5181a43172e62a971855ec5e0fc94e20d08bf37
alexret <alex.relano18@gmail.com>
2020-10-30 12:55:39 +0100

    Falta arreglar session y corregir puntuacion

--- src/main/java/com/example/springquiz/ControladorSession.java
+++ src/main/java/com/example/springquiz/ControladorSession.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,41 +16,99 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ControladorSession {
-	@GetMapping("/session")
-	public String process(Model model, HttpSession session) {
-		@SuppressWarnings("unchecked")
-		List<String> messages = (List<String>) session.getAttribute("respuesta");
-		if (messages == null) {
-		messages = new ArrayList<>();
-		}
-		model.addAttribute("sessionMessages", messages);
-		return "redirect:/prueba";
-	}
 	
 	
+	@Autowired
+	private preguntaDAO p;
 	
-	@PostMapping("/persistMessage")
-		public String persistMessage(@RequestParam String usuario, Model modelo,HttpServletRequest request) {
-		
+	private int puntos;
+	
+	@PostMapping("/inicio")
+	public String persistMessage0(@RequestParam("usuario") String usuario, Model modelo, HttpServletRequest request) {
+		puntos = 0;
 		if (usuario.isEmpty() || usuario.isBlank() || usuario == null) {
 			modelo.addAttribute("error", "Rellene el campo del nombre");
 			return "home";
 		}
 		
-	@SuppressWarnings("unchecked")
-		List<String> messages = (List<String>) request.getSession().getAttribute(usuario);
-		if (messages == null) {
-			messages = new ArrayList<>();
-			request.getSession().setAttribute(usuario, messages);
-		} else
-			messages.add(usuario);
-		
 		request.getSession().setAttribute("usuario", usuario);
 		return "redirect:/index";
+	}	
+	
+	
+	@PostMapping("/pregunta1")
+	public String pregunta1(@RequestParam String respuesta1, Model modelo, HttpServletRequest request) {
+		if (request.getParameter("respuesta1") == "Chuck Norris")
+			puntos++;
+		else
+			puntos--;
+		return "redirect:/index2";
+	}
+	
+	
+	@PostMapping("/pregunta2")
+	public String pregunta2(@RequestParam String respuesta2, Model modelo, HttpServletRequest request) {
+		
+		if (request.getParameter("respuesta2") == "Messi")
+			puntos++;
+		else
+			puntos--;
+		return "redirect:/index3";
+	}
+	
+	@PostMapping("/pregunta3")
+	public String pregunta3(@RequestParam String respuesta3, Model modelo, HttpServletRequest request) {
+		
+		if (request.getParameter("respuesta3").toUpperCase() == "TARTA")
+			puntos++;
+		else
+			puntos--;
+		return "redirect:/index4";
+	}
+	
+	@PostMapping("/pregunta4")
+	public String pregunta4(@RequestParam String respuesta4, Model modelo, HttpServletRequest request) {
+		
+		if (request.getParameter("respuesta4") == "Vaso, Leche, Manos")
+			puntos++;
+		else
+			puntos--;
+		return "redirect:/index5";
+	}
+	
+	@PostMapping("/pregunta5")
+	public String pregunta5(@RequestParam String respuesta5, Model modelo, HttpServletRequest request) {
+		
+		if (request.getParameter("respuesta5") == "Pikachu")
+			puntos++;
+		else
+			puntos--;
+		return "redirect:/index6";
+	}
+	
+	@PostMapping("/pregunta6")
+	public void pregunta6(@RequestParam String respuesta6, Model modelo, HttpServletRequest request) {
+		
+		if (request.getParameter("respuesta6").length() > 0)
+			puntos++;
+		else
+			puntos--;
+		resultado(modelo, request);
+	}
+	
+	
+	private String resultado(Model modelo, HttpServletRequest session) {
+		
+		modelo.addAttribute("puntuacion", puntos);
+		String nombre = (String) session.getAttribute("usuario");
+		p.save(nombre, puntos);
+		
+		return "resultado";
 	}
 	
 	@PostMapping("/destroy")
 		public String destroySession(HttpServletRequest request) {
+		puntos = 0;
 		request.getSession().invalidate();
 		return "redirect:/home";
 	}
--- src/main/java/com/example/springquiz/IndexController.java
+++ src/main/java/com/example/springquiz/IndexController.java
@@ -42,98 +42,161 @@ public class IndexController {
 	
 	
 	@RequestMapping(value="/index", method=RequestMethod.GET)
-	  public String  get_valor(Model modelo, HttpSession session) {
-		cuenta = 0;
-		correccion = 0;
-		rCorrecta = "";
-		preguntas = 0;
-		aux = new ArrayList<>();
-		nombre = (String) session.getAttribute("usuario");
-	    return obtenerModelo(modelo);
+	  public String  get_valor(Model modelo) {
+//		cuenta = 0;
+//		correccion = 0;
+//		rCorrecta = "";
+//		preguntas = 0;
+//		aux = new ArrayList<>();
+//		nombre = (String) session.getAttribute("usuario");
+		pregunta1(modelo);
+	    return "index";
 	}
 	
-	@RequestMapping(value="/prueba", method=RequestMethod.POST) 
-	public String  get_siguiente(Model modelo, 
-								@RequestParam String respuesta) {
-		
-		corregirPregunta(respuesta);
-		
-		return obtenerModelo(modelo);
+	@RequestMapping(value="/index2", method=RequestMethod.GET)
+	  public String  get_valor2(Model modelo) {
+//		cuenta = 0;
+//		correccion = 0;
+//		rCorrecta = "";
+//		preguntas = 0;
+//		aux = new ArrayList<>();
+//		nombre = (String) session.getAttribute("usuario");
+		pregunta2(modelo);
+	    return "pregunta2";
 	}
 	
-	private String obtenerModelo(Model modelo) {
-		if (cuenta == 0) {
-			setValores();
-		}
-		
-		preguntas++;
-		
-		if (preguntas > cuenta) {
-			addPuntuacion(modelo);
-			return "prueba";
-		}
-				
-		int valorDado = obtenerValor();
-		
-		if(valorDado == 0) {
-			modelo = pregunta1(modelo);
-		} else if (valorDado == 1) 
-			modelo = pregunta2(modelo);
-		else if (valorDado == 2) 
-			modelo = pregunta3(modelo);
-		else if (valorDado == 3) 
-			modelo = pregunta4(modelo);
-		else if (valorDado == 4) 
-			modelo = pregunta5(modelo);
-		else if (valorDado == 5) 
-			modelo = pregunta6(modelo);
-		
-		return pagina;
+	@RequestMapping(value="/index3", method=RequestMethod.GET)
+	  public String  get_valor3(Model modelo) {
+//		cuenta = 0;
+//		correccion = 0;
+//		rCorrecta = "";
+//		preguntas = 0;
+//		aux = new ArrayList<>();
+//		nombre = (String) session.getAttribute("usuario");
+		pregunta3(modelo);
+	    return "pregunta3";
 	}
 	
-	private int obtenerValor() {
-		Random r = new Random();
-		
-		int aux_int= r.nextInt(cuenta);
-		
-		if (aux.contains(aux_int)) {
-			
-			while (aux.contains(aux_int))
-				aux_int = r.nextInt(cuenta);
-		}
-		
-		aux.add(aux_int);
-		
-		return aux_int;
+	@RequestMapping(value="/index4", method=RequestMethod.GET)
+	  public String  get_valor4(Model modelo) {
+//		cuenta = 0;
+//		correccion = 0;
+//		rCorrecta = "";
+//		preguntas = 0;
+//		aux = new ArrayList<>();
+//		nombre = (String) session.getAttribute("usuario");
+		pregunta4(modelo);
+	    return "pregunta4";
 	}
 	
-	private void setValores() {
-		File carpeta = new File("src\\main\\resources\\static\\images"); 
-		File[] lista = carpeta.listFiles();    
-		
-		for (int i = 0; i < lista.length; i++) 
-		      if (lista[i].isFile())
-		    	  cuenta++;
+	@RequestMapping(value="/index5", method=RequestMethod.GET)
+	  public String  get_valor5(Model modelo) {
+//		cuenta = 0;
+//		correccion = 0;
+//		rCorrecta = "";
+//		preguntas = 0;
+//		aux = new ArrayList<>();
+//		nombre = (String) session.getAttribute("usuario");
+		pregunta5(modelo);
+	    return "pregunta5";
+	}
+	
+	@RequestMapping(value="/index6", method=RequestMethod.GET)
+	  public String  get_valor6(Model modelo) {
+//		cuenta = 0;
+//		correccion = 0;
+//		rCorrecta = "";
+//		preguntas = 0;
+//		aux = new ArrayList<>();
+//		nombre = (String) session.getAttribute("usuario");
+		pregunta6(modelo);
+	    return "pregunta6";
 	}
 	
 	
-	private void corregirPregunta(String respuesta) {
-		if (respuesta.toUpperCase().equals(rCorrecta.toUpperCase()))
-			correccion++;
-		else
-			correccion--;
-		
-		
-		if (correccion <= 0)
-			correccion = 0;
-		
-	}
 	
-	private Model addPuntuacion(Model modelo) {
-		p.save(nombre, correccion);
-		modelo.addAttribute("correccion", correccion);
-		return modelo;
-	}
+//	@RequestMapping(value="/prueba", method=RequestMethod.POST) 
+//	public String  get_siguiente(Model modelo, 
+//								@RequestParam String respuesta) {
+//		
+//		corregirPregunta(respuesta);
+//		
+//		return obtenerModelo(modelo);
+//	}
+	
+//	private String obtenerModelo(Model modelo) {
+//		if (cuenta == 0) {
+//			setValores();
+//		}
+//		
+//		preguntas++;
+//		
+//		if (preguntas > cuenta) {
+//			addPuntuacion(modelo);
+//			return "prueba";
+//		}
+//				
+//		int valorDado = obtenerValor();
+//		
+//		if(valorDado == 0) {
+//			modelo = pregunta1(modelo);
+//		} else if (valorDado == 1) 
+//			modelo = pregunta2(modelo);
+//		else if (valorDado == 2) 
+//			modelo = pregunta3(modelo);
+//		else if (valorDado == 3) 
+//			modelo = pregunta4(modelo);
+//		else if (valorDado == 4) 
+//			modelo = pregunta5(modelo);
+//		else if (valorDado == 5) 
+//			modelo = pregunta6(modelo);
+//		
+//		return pagina;
+//	}
+//	
+//	private int obtenerValor() {
+//		Random r = new Random();
+//		
+//		int aux_int= r.nextInt(cuenta);
+//		
+//		if (aux.contains(aux_int)) {
+//			
+//			while (aux.contains(aux_int))
+//				aux_int = r.nextInt(cuenta);
+//		}
+//		
+//		aux.add(aux_int);
+//		
+//		return aux_int;
+//	}
+//	
+//	private void setValores() {
+//		File carpeta = new File("src\\main\\resources\\static\\images"); 
+//		File[] lista = carpeta.listFiles();    
+//		
+//		for (int i = 0; i < lista.length; i++) 
+//		      if (lista[i].isFile())
+//		    	  cuenta++;
+//	}
+//	
+//	
+//	private void corregirPregunta(String respuesta) {
+//		if (respuesta.toUpperCase().equals(rCorrecta.toUpperCase()))
+//			correccion++;
+//		else
+//			correccion--;
+//		
+//		
+//		if (correccion <= 0)
+//			correccion = 0;
+//		
+//	}
+//	
+//	private Model addPuntuacion(Model modelo) {
+//		p.save(nombre, correccion);
+//		modelo.addAttribute("correccion", correccion);
+//		return modelo;
+//	}
 	
 	private Model pregunta1(Model modelo) {
 		modelo.addAttribute("foto", "images/chuck.jpg");
@@ -141,9 +204,9 @@ public class IndexController {
 		modelo.addAttribute("pregunta2", "Pascual");
 		modelo.addAttribute("pregunta3", "Messi");
 		modelo.addAttribute("pregunta4", "Chuck Norris");
-		modelo.addAttribute("correccion", correccion);
-		rCorrecta = "Chuck Norris";
-		pagina = "index";
+//		modelo.addAttribute("correccion", correccion);
+//		rCorrecta = "Chuck Norris";
+//		pagina = "index";
 		return modelo;
 	}
 	
@@ -153,53 +216,63 @@ public class IndexController {
 		modelo.addAttribute("pregunta2", "Superman");
 		modelo.addAttribute("pregunta3", "Messi");
 		modelo.addAttribute("pregunta4", "Batman");
-		modelo.addAttribute("correccion", correccion);
-		rCorrecta = "Messi";
-		pagina = "index";
+//		modelo.addAttribute("correccion", correccion);
+//		rCorrecta = "Messi";
+//		pagina = "index";
 		return modelo;
 	}
 	
+//	private Model pregunta3(Model modelo) {
+//		modelo.addAttribute("foto", "images/totoro.png");
+//		modelo.addAttribute("pregunta1", "Totoro");
+//		modelo.addAttribute("pregunta2", "Hulk");
+//		modelo.addAttribute("pregunta3", "Charmander");
+//		modelo.addAttribute("pregunta4", "Bill Gates");
+//		modelo.addAttribute("correccion", correccion);
+//		rCorrecta = "Totoro";
+//		pagina = "index";
+//		return modelo;
+//	}
+//	
+
+	
 	private Model pregunta3(Model modelo) {
-		modelo.addAttribute("foto", "images/totoro.png");
-		modelo.addAttribute("pregunta1", "Totoro");
-		modelo.addAttribute("pregunta2", "Hulk");
-		modelo.addAttribute("pregunta3", "Charmander");
-		modelo.addAttribute("pregunta4", "Bill Gates");
-		modelo.addAttribute("correccion", correccion);
-		rCorrecta = "Totoro";
-		pagina = "index";
+		modelo.addAttribute("foto", "images/tarta.jpg");
+//		modelo.addAttribute("correccion", correccion);
+//		rCorrecta = "Tarta";
+//		pagina = "preguntatext";
 		return modelo;
 	}
 	
 	private Model pregunta4(Model modelo) {
-		modelo.addAttribute("foto", "images/pikachu.png");
-		modelo.addAttribute("pregunta1", "Un ordenador");
-		modelo.addAttribute("pregunta2", "Pikachu");
-		modelo.addAttribute("pregunta3", "The Rock");
-		modelo.addAttribute("pregunta4", "Jackie Chan");
-		modelo.addAttribute("correccion", correccion);
-		rCorrecta = "Pikachu";
-		pagina = "index";
-		return modelo;
-	}
-	
-	private Model pregunta5(Model modelo) {
-		modelo.addAttribute("foto", "images/tarta.jpg");
-		modelo.addAttribute("correccion", correccion);
-		rCorrecta = "Tarta";
-		pagina = "preguntatext";
-		return modelo;
-	}
-	
-	private Model pregunta6(Model modelo) {
 		modelo.addAttribute("foto", "images/leche.jpg");
 		modelo.addAttribute("pregunta1", "Vaso");
 		modelo.addAttribute("pregunta2", "Leche");
 		modelo.addAttribute("pregunta3", "Manos");
 		modelo.addAttribute("pregunta4", "No se ve nada");
-		modelo.addAttribute("correccion", correccion);
-		rCorrecta = "Vaso, Leche, Manos";
-		pagina = "preguntacheck";
+//		modelo.addAttribute("correccion", correccion);
+//		rCorrecta = "Vaso, Leche, Manos";
+//		pagina = "preguntacheck";
+		return modelo;
+	}
+	
+	private Model pregunta5(Model modelo) {
+		modelo.addAttribute("foto", "images/pikachu.png");
+		modelo.addAttribute("pregunta1", "Un ordenador");
+		modelo.addAttribute("pregunta2", "Pikachu");
+		modelo.addAttribute("pregunta3", "The Rock");
+		modelo.addAttribute("pregunta4", "Jackie Chan");
+//		modelo.addAttribute("correccion", correccion);
+//		rCorrecta = "Pikachu";
+//		pagina = "index";
+		return modelo;
+	}
+	
+	private Model pregunta6(Model modelo) {
+		modelo.addAttribute("foto", "images/totoro.png");
+//		modelo.addAttribute("correccion", correccion);
+//		rCorrecta = "Pikachu";
+//		pagina = "index";
 		return modelo;
 	}
 
